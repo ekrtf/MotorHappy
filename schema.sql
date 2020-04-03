0c9ee849407421709fd908516f0d7f8e1d9ff7aa
Emile Kratiroff <emile@cryptio.co>
2020-04-03 19:22:33 +0200

    feat: draft schema in SQL file

--- schema.sql
+++ schema.sql
@@ -3,12 +3,20 @@
 CREATE DATABASE automobile_api;
 
 CREATE TABLE makes (
-    id uuid,
+    make_id uuid,
     name text,
     founded_at date,
-    founded_in text,
+    founded_in text
+);
+
+CREATE TABLE vehicle_types (
+    vehicle_type_id uuid,
+    name text
 );
 
 CREATE TABLE vehicles (
-
+    vehicle_id uuid,
+    vehicle_type_id uuid
+    make_id uuid,
+    model_name text
 );
