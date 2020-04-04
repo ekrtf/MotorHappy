b7f5381a072eaf292bb14544524edee5dcc8c96d
Emile Kratiroff <emile@cryptio.co>
2020-04-04 02:45:33 +0200

    feat: connecting to postgres

---
+++ init.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE automobile_api;
--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+var db *gorm.DB
+
+func init() {
+	//open a db connection
+	var err error
+	// TODO: use env
+	// TODO: enable ssl
+	db, err = gorm.Open("postgres", "host=localhost port=5454 user=postgres dbname=automobile_api password=pw sslmode=disable")
+	if err != nil {
+		fmt.Print(err)
+		panic("failed to connect database")
+	}
+}
 
 func main() {
 	r := gin.Default()
